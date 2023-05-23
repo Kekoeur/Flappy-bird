cb817a9cfaa8d42a3d95828ddaffa743243b7ddd
Christopher <christo26.g@gmail.com>
2023-05-23 10:31:25 +0200

    mobil

--- src/common/common.js
+++ src/common/common.js
@@ -1,5 +1,5 @@
-export var width = window.innerWidth;
-export var height = window.innerHeight;
+export var width = window.innerWidth-2;
+export var height = window.innerHeight-2;
 
 if (width >= 500) { //if this desktop
   width  = 320;
